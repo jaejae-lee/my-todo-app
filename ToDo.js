f763c34db47f0aef3fee0b41c5ee04ace1e91fa5
jae.L <jklee900414@gmail.com>
2020-01-17 19:53:26 +0000

    event stopPropagation added to fix the event bubbles up and app loading bg color changed on app.json

--- ToDo.js
+++ ToDo.js
@@ -70,7 +70,7 @@ class ToDo extends Component {
                                 <Text style={ styles.actionText }>✏️</Text>
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity onPressOut={ () => deleteToDo(id) }>
+                        <TouchableOpacity onPressOut={ (event) =>  { event.stopPropagation; deleteToDo(id) } }>
                             <View style={ styles.actionContainer }>
                                 <Text style={ styles.actionText }>❌️️</Text>
                             </View>
@@ -81,7 +81,8 @@ class ToDo extends Component {
          );     
     }
 
-    toggleDone = () => {
+    toggleDone = (event) => {
+        event.stopPropagation();
         const { isDone, unDoneToDo, doneToDo, id } = this.props;
         if(isDone){
             unDoneToDo(id)
@@ -89,12 +90,14 @@ class ToDo extends Component {
             doneToDo(id)
         }
     }
-    startEditing = () => {
+    startEditing = (event) => {
+        event.stopPropagation();
         this.setState({
             isEditing: true,
         })
     }
-    doneEditing = () => {
+    doneEditing = (event) => {
+        event.stopPropagation();
         const { toDoValue } = this.state;
         const { id, updateToDo } = this.props;
         updateToDo(id, toDoValue)
